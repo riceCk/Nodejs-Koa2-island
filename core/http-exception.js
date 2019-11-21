9c625a4bec2bec902d917942ff18444daaa14c78
riceCk <876496731@qq.com>
2019-11-21 22:59:45 +0800

    全局异常处理规则

--- core/http-exception.js
+++ core/http-exception.js
@@ -52,11 +52,32 @@ class Forbbiden extends HttpException {
   }
 }
 
+class LikeError extends HttpException{
+  constructor(msg, errorCode) {
+    super()
+	this.code = 400;
+	this.msg = '你已经点赞过';
+	this.errorCode = errorCode || 60001
+  }
+}
+
+class DislikeError extends HttpException{
+  constructor(msg, errorCode) {
+	super()
+	this.code = 400;
+	this.msg = '你已取消点赞过';
+	this.errorCode = errorCode || 60002
+  }
+}
+
+
 module.exports = {
   HttpException,
   ParameterException,
   Success,
   NotFound,
   AuthFiled,
-  Forbbiden
+  Forbbiden,
+  LikeError,
+  DislikeError
 }
